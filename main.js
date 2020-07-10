d67fead70b11b42f002931ff9dc06507a1c4c156
azamiya <t.adaniya@adawarp.com>
2020-07-10 12:41:59 +0900

    chore: remove motor config

--- main.js
+++ main.js
@@ -5,22 +5,9 @@ const board = new Board();
 const PWM_RANGE_MIN = 560
 const PWM_RANGE_MAX = 2480
 
-const TB6612_AIN1 = 0
-const TB6612_AIN2 = 1
-const TB6612_BIN1 = 2
-const TB6612_BIN2 = 3
-const TB6612_STBY = 8
-
 const cal = (x) => five.Fn.map(x, 0, 320, 0, 288)
 
 board.on("ready", () => {
-
-  board.pinMode(TB6612_AIN1, board.MODES.OUTPUT);
-  board.pinMode(TB6612_AIN2, board.MODES.OUTPUT);
-  board.pinMode(TB6612_BIN1, board.MODES.OUTPUT);
-  board.pinMode(TB6612_BIN2, board.MODES.OUTPUT);
-  board.pinMode(TB6612_STBY, board.MODES.OUTPUT);
-
   console.log(board)
   const servo1 = new five.Servo({
     controller: "PCA9685",
@@ -63,32 +50,5 @@ board.on("ready", () => {
   setTimeout(() => {
     servo2.to(cal(90))
   }, 10000)
-
-  const leftMotor = new Motor({
-    controller: "PCA9685",
-    pins: {
-      pwm: 6,
-      dir: TB6612_AIN1,
-      cdir: TB6612_AIN2
-    }
-  });
-
-  board.digitalWrite(TB6612_AIN1, 0);
-  board.digitalWrite(TB6612_AIN2, 1);
-  leftMotor.speed(150);
-
-  setTimeout(() => {
-    leftMotor.stop();
-  }, 2000)
-
-  setTimeout(() => {
-    board.digitalWrite(TB6612_AIN1, 1);
-    board.digitalWrite(TB6612_AIN2, 0);
-    leftMotor.speed(100);
-  }, 4000)
-
-  setTimeout(() => {
-    leftMotor.stop();
-  }, 6000)
 
 });
